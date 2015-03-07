9077b145f3efd9261df02f57f5b2a9834385ca17
viral666 <braiant@o2.pl>
2015-03-07 09:12:04 +0100

    Nowa_Faktura

---
+++ Warehouse Management System/NowaFakturaForm.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace Warehouse_Management_System
+{
+    partial class NowaFakturaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKlientNip = new System.Windows.Forms.TextBox();
+            this.txtKlientUlica = new System.Windows.Forms.TextBox();
+            this.txtKlientNazwa = new System.Windows.Forms.TextBox();
+            this.txtKlientKod = new System.Windows.Forms.TextBox();
+            this.txtKlientMiasto = new System.Windows.Forms.TextBox();
+            this.txtFakturaNr = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimeDataZ = new System.Windows.Forms.DateTimePicker();
+            this.dateTimeDataW = new System.Windows.Forms.DateTimePicker();
+            this.dateTimeDataS = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridNowaFaktura = new System.Windows.Forms.DataGridView();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnWybierzKlienta = new System.Windows.Forms.Button();
+            this.btnAnuluj = new System.Windows.Forms.Button();
+            this.btnZapisz = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridNowaFaktura)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtKlientNip
+            // 
+            this.txtKlientNip.Location = new System.Drawing.Point(112, 180);
+            this.txtKlientNip.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKlientNip.Name = "txtKlientNip";
+            this.txtKlientNip.Size = new System.Drawing.Size(432, 22);
+            this.txtKlientNip.TabIndex = 31;
+            // 
+            // txtKlientUlica
+            // 
+            this.txtKlientUlica.Location = new System.Drawing.Point(110, 113);
+            this.txtKlientUlica.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKlientUlica.Name = "txtKlientUlica";
+            this.txtKlientUlica.Size = new System.Drawing.Size(432, 22);
+            this.txtKlientUlica.TabIndex = 30;
+            // 
+            // txtKlientNazwa
+            // 
+            this.txtKlientNazwa.Location = new System.Drawing.Point(110, 81);
+            this.txtKlientNazwa.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKlientNazwa.Name = "txtKlientNazwa";
+            this.txtKlientNazwa.Size = new System.Drawing.Size(432, 22);
+            this.txtKlientNazwa.TabIndex = 29;
+            // 
+            // txtKlientKod
+            // 
+            this.txtKlientKod.Location = new System.Drawing.Point(110, 150);
+            this.txtKlientKod.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKlientKod.Name = "txtKlientKod";
+            this.txtKlientKod.Size = new System.Drawing.Size(95, 22);
+            this.txtKlientKod.TabIndex = 28;
+            // 
+            // txtKlientMiasto
+            // 
+            this.txtKlientMiasto.Location = new System.Drawing.Point(289, 147);
+            this.txtKlientMiasto.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKlientMiasto.Name = "txtKlientMiasto";
+            this.txtKlientMiasto.Size = new System.Drawing.Size(253, 22);
+            this.txtKlientMiasto.TabIndex = 27;
+            // 
+            // txtFakturaNr
+            // 
+            this.txtFakturaNr.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtFakturaNr.Location = new System.Drawing.Point(112, 26);
+            this.txtFakturaNr.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFakturaNr.Name = "txtFakturaNr";
+            this.txtFakturaNr.ReadOnly = true;
+            this.txtFakturaNr.Size = new System.Drawing.Size(263, 23);
+            this.txtFakturaNr.TabIndex = 26;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(25, 180);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(33, 17);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Nip:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 147);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 17);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Kod:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 116);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 17);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Ulica:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(228, 148);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 17);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Miasto:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 84);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 17);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Nazwa:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 63);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Klient:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Nr Faktury:";
+            // 
+            // dateTimeDataZ
+            // 
+            this.dateTimeDataZ.CustomFormat = "dd-MM-yyyy";
+            this.dateTimeDataZ.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimeDataZ.Location = new System.Drawing.Point(884, 90);
+            this.dateTimeDataZ.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimeDataZ.Name = "dateTimeDataZ";
+            this.dateTimeDataZ.Size = new System.Drawing.Size(191, 22);
+            this.dateTimeDataZ.TabIndex = 37;
+            // 
+            // dateTimeDataW
+            // 
+            this.dateTimeDataW.CustomFormat = "dd-MM-yyyy";
+            this.dateTimeDataW.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimeDataW.Location = new System.Drawing.Point(884, 58);
+            this.dateTimeDataW.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimeDataW.Name = "dateTimeDataW";
+            this.dateTimeDataW.Size = new System.Drawing.Size(191, 22);
+            this.dateTimeDataW.TabIndex = 36;
+            // 
+            // dateTimeDataS
+            // 
+            this.dateTimeDataS.CustomFormat = "dd-MM-yyyy";
+            this.dateTimeDataS.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimeDataS.Location = new System.Drawing.Point(884, 26);
+            this.dateTimeDataS.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimeDataS.Name = "dateTimeDataS";
+            this.dateTimeDataS.Size = new System.Drawing.Size(191, 22);
+            this.dateTimeDataS.TabIndex = 35;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(730, 99);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Termin zapłaty:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(730, 67);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Data wystawienia:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(730, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Data sprzedaży:";
+            // 
+            // dataGridNowaFaktura
+            // 
+            this.dataGridNowaFaktura.AllowUserToAddRows = false;
+            this.dataGridNowaFaktura.AllowUserToDeleteRows = false;
+            this.dataGridNowaFaktura.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridNowaFaktura.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridNowaFaktura.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridNowaFaktura.Location = new System.Drawing.Point(43, 248);
+            this.dataGridNowaFaktura.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridNowaFaktura.Name = "dataGridNowaFaktura";
+            this.dataGridNowaFaktura.ReadOnly = true;
+            this.dataGridNowaFaktura.Size = new System.Drawing.Size(993, 254);
+            this.dataGridNowaFaktura.TabIndex = 39;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(505, 213);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(61, 17);
+            this.label11.TabIndex = 38;
+            this.label11.Text = "Pozycje:";
+            // 
+            // btnWybierzKlienta
+            // 
+            this.btnWybierzKlienta.Location = new System.Drawing.Point(733, 156);
+            this.btnWybierzKlienta.Margin = new System.Windows.Forms.Padding(4);
+            this.btnWybierzKlienta.Name = "btnWybierzKlienta";
+            this.btnWybierzKlienta.Size = new System.Drawing.Size(255, 41);
+            this.btnWybierzKlienta.TabIndex = 40;
+            this.btnWybierzKlienta.Text = "Wybierz klienta";
+            this.btnWybierzKlienta.UseVisualStyleBackColor = true;
+            // 
+            // btnAnuluj
+            // 
+            this.btnAnuluj.Location = new System.Drawing.Point(43, 565);
+            this.btnAnuluj.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAnuluj.Name = "btnAnuluj";
+            this.btnAnuluj.Size = new System.Drawing.Size(133, 41);
+            this.btnAnuluj.TabIndex = 43;
+            this.btnAnuluj.Text = "Anuluj";
+            this.btnAnuluj.UseVisualStyleBackColor = true;
+            // 
+            // btnZapisz
+            // 
+            this.btnZapisz.Location = new System.Drawing.Point(903, 565);
+            this.btnZapisz.Margin = new System.Windows.Forms.Padding(4);
+            this.btnZapisz.Name = "btnZapisz";
+            this.btnZapisz.Size = new System.Drawing.Size(133, 41);
+            this.btnZapisz.TabIndex = 42;
+            this.btnZapisz.Text = "Zapisz";
+            this.btnZapisz.UseVisualStyleBackColor = true;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(744, 565);
+            this.btnDodaj.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(133, 41);
+            this.btnDodaj.TabIndex = 41;
+            this.btnDodaj.Text = "Dodaj pozycje";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // NowaFakturaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 619);
+            this.Controls.Add(this.btnAnuluj);
+            this.Controls.Add(this.btnZapisz);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.btnWybierzKlienta);
+            this.Controls.Add(this.dataGridNowaFaktura);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.dateTimeDataZ);
+            this.Controls.Add(this.dateTimeDataW);
+            this.Controls.Add(this.dateTimeDataS);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKlientNip);
+            this.Controls.Add(this.txtKlientUlica);
+            this.Controls.Add(this.txtKlientNazwa);
+            this.Controls.Add(this.txtKlientKod);
+            this.Controls.Add(this.txtKlientMiasto);
+            this.Controls.Add(this.txtFakturaNr);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Name = "NowaFakturaForm";
+            this.Text = "Nowa Faktura";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridNowaFaktura)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKlientNip;
+        private System.Windows.Forms.TextBox txtKlientUlica;
+        private System.Windows.Forms.TextBox txtKlientNazwa;
+        private System.Windows.Forms.TextBox txtKlientKod;
+        private System.Windows.Forms.TextBox txtKlientMiasto;
+        private System.Windows.Forms.TextBox txtFakturaNr;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimeDataZ;
+        private System.Windows.Forms.DateTimePicker dateTimeDataW;
+        private System.Windows.Forms.DateTimePicker dateTimeDataS;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridNowaFaktura;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnWybierzKlienta;
+        private System.Windows.Forms.Button btnAnuluj;
+        private System.Windows.Forms.Button btnZapisz;
+        private System.Windows.Forms.Button btnDodaj;
+    }
+}
