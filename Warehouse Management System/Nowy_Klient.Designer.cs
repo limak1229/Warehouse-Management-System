87c40fddd21bed26614d20d5125112806f497987
viral666 <braiant@o2.pl>
2015-03-07 08:12:16 +0100

    Okno Nowy_Klient

---
+++ Warehouse Management System/Nowy_Klient.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Warehouse_Management_System
+{
+    partial class Nowy_Klient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKlienciMiasto = new System.Windows.Forms.TextBox();
+            this.txtKlienciKod = new System.Windows.Forms.TextBox();
+            this.txtKlienciUlica = new System.Windows.Forms.TextBox();
+            this.txtKlienciNazwa = new System.Windows.Forms.TextBox();
+            this.txtKlienciNip = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAnuluj = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtKlienciMiasto
+            // 
+            this.txtKlienciMiasto.Location = new System.Drawing.Point(119, 161);
+            this.txtKlienciMiasto.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKlienciMiasto.Name = "txtKlienciMiasto";
+            this.txtKlienciMiasto.Size = new System.Drawing.Size(291, 22);
+            this.txtKlienciMiasto.TabIndex = 10;
+            // 
+            // txtKlienciKod
+            // 
+            this.txtKlienciKod.Location = new System.Drawing.Point(119, 130);
+            this.txtKlienciKod.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKlienciKod.Name = "txtKlienciKod";
+            this.txtKlienciKod.Size = new System.Drawing.Size(148, 22);
+            this.txtKlienciKod.TabIndex = 9;
+            // 
+            // txtKlienciUlica
+            // 
+            this.txtKlienciUlica.Location = new System.Drawing.Point(119, 96);
+            this.txtKlienciUlica.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKlienciUlica.Name = "txtKlienciUlica";
+            this.txtKlienciUlica.Size = new System.Drawing.Size(291, 22);
+            this.txtKlienciUlica.TabIndex = 8;
+            // 
+            // txtKlienciNazwa
+            // 
+            this.txtKlienciNazwa.Location = new System.Drawing.Point(119, 63);
+            this.txtKlienciNazwa.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKlienciNazwa.Name = "txtKlienciNazwa";
+            this.txtKlienciNazwa.Size = new System.Drawing.Size(291, 22);
+            this.txtKlienciNazwa.TabIndex = 7;
+            // 
+            // txtKlienciNip
+            // 
+            this.txtKlienciNip.Location = new System.Drawing.Point(119, 30);
+            this.txtKlienciNip.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKlienciNip.Name = "txtKlienciNip";
+            this.txtKlienciNip.Size = new System.Drawing.Size(291, 22);
+            this.txtKlienciNip.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 166);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Miasto:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 133);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Kod:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 100);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Ulica:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nazwa:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "NIP:";
+            // 
+            // btnAnuluj
+            // 
+            this.btnAnuluj.Location = new System.Drawing.Point(17, 260);
+            this.btnAnuluj.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAnuluj.Name = "btnAnuluj";
+            this.btnAnuluj.Size = new System.Drawing.Size(133, 41);
+            this.btnAnuluj.TabIndex = 17;
+            this.btnAnuluj.Text = "Anuluj";
+            this.btnAnuluj.UseVisualStyleBackColor = true;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(277, 260);
+            this.btnDodaj.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(133, 41);
+            this.btnDodaj.TabIndex = 16;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // Nowy_Klient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 399);
+            this.Controls.Add(this.btnAnuluj);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKlienciMiasto);
+            this.Controls.Add(this.txtKlienciKod);
+            this.Controls.Add(this.txtKlienciUlica);
+            this.Controls.Add(this.txtKlienciNazwa);
+            this.Controls.Add(this.txtKlienciNip);
+            this.Name = "Nowy_Klient";
+            this.Text = "Nowy_Klient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKlienciMiasto;
+        private System.Windows.Forms.TextBox txtKlienciKod;
+        private System.Windows.Forms.TextBox txtKlienciUlica;
+        private System.Windows.Forms.TextBox txtKlienciNazwa;
+        private System.Windows.Forms.TextBox txtKlienciNip;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAnuluj;
+        private System.Windows.Forms.Button btnDodaj;
+    }
+}
